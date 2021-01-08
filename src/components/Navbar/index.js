963a40f48d84e9c9fd00d9f557082fe49b7d90f1
igor abramenkov <igorek699669@gmail.com>
2021-01-08 15:31:36 +0300

    add project

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav className='nav'>
+            <ul>
+                <li>
+                    <a href="">
+                        profile
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        messages
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        news
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        music
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        settings
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    )
+};
+export {
+    Navbar
+}
---
+++ src/components/Profile/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src="https://theinpaint.com/images/example-1-2.jpg" alt=""/>
+            </div>
+            <img src="https://dyl80ryjxr1ke.cloudfront.net/external_assets/hero_examples/hair_beach_v1785392215/original.jpeg" alt=""/>
+        </div>
+    )
+};
+export {
+    Profile
+}
