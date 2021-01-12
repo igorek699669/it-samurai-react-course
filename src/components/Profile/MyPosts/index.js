16ac85b8698de5140bde0f90c2dc9c6dd3b253a2
igor abramenkov <igorek699669@gmail.com>
2021-01-12 21:12:32 +0300

    closed 31 lesson

--- src/components/Profile/MyPosts/index.js
+++ src/components/Profile/MyPosts/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 import classes from './index.module.css';
 import {Post} from "./Post";
 const MyPosts = (props) => {
-
-
+    let newPostElement = React.createRef();
+    let addPost = () => {
+        let text = newPostElement.current.value;
+        alert(text)
+    }
     return (
         <div className="posts">
             <div className="header-text">
                 my posts
             </div>
             <div className="add_post">
-                <textarea name=""></textarea>
-                <button>Add post</button>
+                <textarea ref={newPostElement} name=""></textarea>
+                <button onClick={addPost} >Add post</button>
             </div>
             <div className="posts-wrapper">
                 {props.postsData.map((el)=><Post id={el.id} message={el.message} likesCount={el.likesCount}/>)}
