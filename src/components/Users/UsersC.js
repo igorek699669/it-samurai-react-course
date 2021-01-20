1ae98d5884a9b55ddce373ec510f1d39e4b49257
igor abramenkov <igorek699669@gmail.com>
2021-01-20 12:54:04 +0300

    closed 54 lesson

--- src/components/Users/UsersC.js
+++ src/components/Users/UsersC.js
@@ -6,6 +6,8 @@ import userImg from '../../assets/images/avatar.png'
 class Users extends React.Component{
     constructor(props){
         super(props)
+    }
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
             this.props.setUsers(response.data.items)
         })
